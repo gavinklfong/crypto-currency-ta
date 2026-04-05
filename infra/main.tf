023a2b061d67c3c8e14ef1944c8c10d0ef575b3e
Gavin Fong <gavinklfong@gmail.com>
2026-04-05 12:02:01 +0100

    add api gateway

--- infra/main.tf
+++ infra/main.tf
@@ -31,3 +31,59 @@ resource "aws_lambda_function" "hello_world" {
 
   role = aws_iam_role.lambda_exec.arn
 }
+
+
+
+########################################
+# API Gateway HTTP API (v2)
+########################################
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "hello-world-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.hello_world.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "hello_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+########################################
+# Lambda permission for API Gateway
+########################################
+
+resource "aws_lambda_permission" "allow_apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+########################################
+# Output the public URL
+########################################
+
+output "api_invoke_url" {
+  value = aws_apigatewayv2_stage.default_stage.invoke_url
+}
