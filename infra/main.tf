e0a5f7efece5b865ce4673d7c7613413a76a00d4
Gavin Fong <gavinklfong@gmail.com>
2026-04-05 11:12:29 +0100

    update lambda function name

--- infra/main.tf
+++ infra/main.tf
@@ -2,21 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
-# resource "aws_s3_bucket" "tf_state" {
-#   bucket        = "lambda-terraform-state-bucket-164995166068-us-east-2-an"
-#   force_destroy = false
-# }
-
-# # Remote state configuration
-# resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-#   bucket = aws_s3_bucket.tf_state.id
-
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
 # Lambda function and execution role
 resource "aws_iam_role" "lambda_exec" {
   name = "hello-world-lambda-exec"
@@ -37,7 +22,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 }
 
 resource "aws_lambda_function" "hello_world" {
-  function_name = "hello-world-lambda"
+  function_name = "hello-world-lambda-updated"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
 
