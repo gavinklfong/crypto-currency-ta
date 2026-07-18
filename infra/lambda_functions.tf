d84dfff0af665783926450a154ea61318da96353
Gavin Fong <gavinklfong@gmail.com>
2026-07-18 15:48:42 +0100

    improve lambda env arrangement

--- infra/lambda_functions.tf
+++ infra/lambda_functions.tf
@@ -3,6 +3,17 @@ locals {
     for key, _ in var.fifo_queues :
     key => aws_sqs_queue.fifo[key].url
   }
+
+  special_lambda_env = {
+    "ec2-job-launcher" = {
+      LAUNCH_TEMPLATE_ID          = aws_launch_template.ec2_worker_lt.id
+      TA_JOB_SCRIPTS_BUCKET_NAME = aws_s3_bucket.ta_job_scripts.id
+    }
+  }
+
+  common_lambda_env = {
+    SNS_TOPIC_ARN = aws_sns_topic.slack_notifications.arn
+  }
 }
 
 resource "aws_lambda_function" "lambda" {
@@ -25,15 +36,10 @@ resource "aws_lambda_function" "lambda" {
 
   environment {
     variables = merge(
+      local.common_lambda_env,
       each.value.environment,
       lookup(var.lambda_env, each.key, {}),
-      each.key == "ec2-job-launcher" ? {
-        LAUNCH_TEMPLATE_ID = aws_launch_template.ec2_worker_lt.id
-        TA_JOB_SCRIPTS_BUCKET_NAME = aws_s3_bucket.ta_job_scripts.id
-      } : {},
-      {
-        SNS_TOPIC_ARN = aws_sns_topic.slack_notifications.arn
-      }
+      lookup(local.special_lambda_env, each.key, {})
     )
   }
 }
